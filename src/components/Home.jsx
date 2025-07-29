107d2a5177d8e9967147a88b0c72cb9d89575ce9
Shireen Khan <066shiree@gmail.com>
2025-07-29 17:18:07 +0530

    Project Done

---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import ImageUpload from "./ImageUpload";
+import ImagePreview from "./ImagePreview";
+import { useState } from "react";
+import { enhancedImageAPI } from "../utils/enhanceImageApi";
+
+const Home = () => {
+    document.title = "AI Image Enhancer";
+    const [uploadImage, setUploadImage] = useState(null);
+    const [enhancedImage, setEnhancedImage] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const UploadImageHandler = async (file) => {
+        setUploadImage(URL.createObjectURL(file));
+        setLoading(true);
+        try {
+            const enhancedURL = await enhancedImageAPI(file);
+            setEnhancedImage(enhancedURL);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            alert("Error while enhancing the image. Please try again later.");
+        }
+    };
+
+    return (
+        <>
+            <ImageUpload UploadImageHandler={UploadImageHandler} />
+            <ImagePreview
+                loading={loading}
+                uploaded={uploadImage}
+                enhanced={enhancedImage?.image}
+            />
+        </>
+    );
+};
+
+export default Home;
